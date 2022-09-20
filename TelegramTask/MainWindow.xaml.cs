b8810a0e8fbb7e278a2274ca30bf56a24b06fc49
User <User@DESKTOP-555P1SP>
2022-09-20 13:29:03 +0400

    Add project files.

---
+++ TelegramTask/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TelegramTask
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            string help = titleLbl.Content.ToString();
+            titleLbl.Content = btn1.Content;
+            btn1.Content=help;
+        }
+    }
+}
